945d4d8a98e344da9bcdeb7329b3fdba7617cf73
kellobri <kellyobriant@gmail.com>
2021-01-04 15:14:04 -0800

    one two three

--- Application1/app.R
+++ Application1/app.R
@@ -13,7 +13,7 @@ library(shiny)
 ui <- fluidPage(
     
     # Application title
-    titlePanel("Old Faithful Geyser Application 1111111"),
+    titlePanel("Old Faithful Geyser Application 222"),
 
     
     # Sidebar with a slider input for number of bins 
